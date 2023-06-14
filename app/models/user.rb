d76792fca56dc70d93c170a4aec1d31c71b7c691
nilpatel <nil.p@simformsolutions.com>
2023-06-14 17:36:34 +0530

    practice

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 10 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_one_attached :avtar
 end
